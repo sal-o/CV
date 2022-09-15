f70929d794fce775d79220c3e390ee6a94aa43d9
sal-o <113565277+sal-o@users.noreply.github.com>
2022-09-14 21:51:32 -0300

    Add Inicial CV webs

---
+++ index.js
@@ -0,0 +1,19 @@
+function saludo() {
+
+    alert("Hey, thanks for coming ")
+
+}
+
+setTimeout(function saludo() {
+   
+}, 5000)
+
+function agradecer() {
+
+    alert("Thanks 🤗 ")
+
+}
+
+setTimeout(function agredecer(){
+
+}, 5000)
